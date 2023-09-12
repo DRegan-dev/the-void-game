962bb2a1197c1a0412b8943882ed506ec39c968f
DRegan-dev <>
2023-09-12 21:05:46 +0000

    added questions array to variable called questions

---
+++ assets/script.js
@@ -0,0 +1,80 @@
+// This event listener runs the function once the HTML document is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+// This is an array of objects, where each object represents a question for the quiz.
+    let questions = [{
+        question: "What 1949 science fiction book by author George Orwell describes a dystopian world in the future?",
+        choices: ["A. Bladerunner", "B. The Stranger", "C. 1984", "D. The Road"],
+        answer: "C"
+      },
+      {
+        question: "Which Italian town is the setting for Shakespeares Romeo and Juliet?",
+        choices: ["A. Rome", "B. Milan", "C. Venice", "D. Verona"],
+        answer: "D"
+      },
+      {
+        question: "Which globally-dreaded disease did the World Health Organization declared eradicated in 1980?",
+        choices: ["A. Sars", "B. Small Pox", "C. Tuberculosis", "D. Man Flu"],
+        answer: "B"
+      },
+      {
+        question: "Who was the first “American Idol” winner?",
+        choices: ["A. Katy Perry", "B. Celine Dion", "C. Ariana Grande", "D. Kelly Clarkson"],
+        answer: "D"
+      },
+      {
+        question: "What movie is 'you had me at hello' from?",
+        choices: ["A. Billy Elliot", "B. Lil Nicky", "C. BraveHeart", "D. Jerry Maguire"],
+        answer: "D"
+      },
+      {
+        question: "Which of the six main characters on the TV show “Friends” never got married?",
+        choices: ["A. Ross", "B. Rachel", "C. Joey", "D. Phoebe"],
+        answer: "C"
+      },
+      {
+        question: "What species of fish is Nemo?",
+        choices: ["A. Pufferfish", "B. Clownfish", "C. Blowfish", "D. Salmon"],
+        answer: "B"
+      },
+      {
+        question: "Where did the 2000 Summer Olympics take place?",
+        choices: ["A. Greece", "B. Australia", "C. Ireland", "D. Russia"],
+        answer: "B"
+      },
+      {
+        question: "What is an eight-sided shape called?",
+        choices: ["A. Hexagon", "B. Rectangle", "C. Decahedron", "A. Octagon"],
+        answer: "D"
+      },
+      {
+        question: "Where is the worlds largest Starbucks?",
+        choices: ["A. New York", "B. Brisbane", "C. Chicago", "D. Tokyo"],
+        answer: "C"
+      },
+      {
+        question: "What is a single strand of spaghetti called?",
+        choices: ["A. Spaghetto", "B. Noodle", "C. String", "D. Tendon"],
+        answer: "A"
+      },
+      {
+        question: "Which NBA team plays its home games at Madison Square Garden?",
+        choices: ["A. Lakers", "B. Bulls", "C. Knicks", "D. Nets"],
+        answer: "C"
+      },
+      {
+        question: "What color is a giraffe’s tongue",
+        choices: ["A. Black", "B. Yellow", "C. Red", "D. Purple"],
+        answer: "D"
+      },
+      {
+        question: "Who wrote “The Little Mermaid”? ",
+        choices: ["A. Walt Disney", "B. Hans C. Anderson", "C. Roald Dahl", "D. Bob Ross"],
+        answer: "B"
+      },
+      {
+        question: "How many ghosts show up in “A Christmas Carol”?",
+        choices: ["A. Three", "B. Two", "C. Four", "D. Five"],
+        answer: "C"
+      }
+    ];
+})
